bd575848e5a11839adf3c646bb44e8ad5b1f773e
Pedro-ui-max <pedrorodrigues0502@gmail.com>
2026-06-08 12:10:17 -0300

    Add files via upload

---
+++ contagem_regressiva/Contagem.cs
@@ -0,0 +1,16 @@
+﻿//declaraçao de variavel
+double numero = 0.0;
+
+//interaçao com o usuario
+
+Console.WriteLine("digite um numero: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+while(numero > 0)
+{
+    numero= numero -1;
+    Console.WriteLine(numero);
+}
+{
+    Console.WriteLine("fim da contagem ");
+}
---
+++ media_escolar/media_escolar.cs
@@ -0,0 +1,28 @@
+﻿//algoritmo para calcular a media escolar
+double nota1 =0.0;
+double nota2 =0.0;
+double nota3 =0.0;
+double nota4 =0.0;
+double media =0.0;
+
+//interaçao com o usuario 
+Console.WriteLine("diga a sua primeira nota: ");
+nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("diga a sua segunda nota: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("diga a sua terceira nota: ");
+nota3 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("diga a sua quarta nota: ");
+nota4 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2 + nota3 + nota4 ) /4;
+
+if(media >=7 ){
+Console.WriteLine("voce foi aprovado: ");
+}
+else{ 
+    Console.WriteLine("voce foi reprovado: ");
+}
