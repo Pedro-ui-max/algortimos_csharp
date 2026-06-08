6b855e7aa6b3083a533894741bdb31498e938bf5
Pedro-ui-max <pedrorodrigues0502@gmail.com>
2026-06-08 12:11:28 -0300

    Add files via upload

---
+++ tabuada/Program.cs
@@ -0,0 +1,21 @@
+﻿//declaraçao de variaveis
+
+double numero = 0.0;
+double multiplicador = 0.0;
+double resultado = numero * multiplicador;
+
+Console.WriteLine("digite um numero");
+ numero = Convert.ToDouble(Console.ReadLine());
+
+
+while(multiplicador <=10)
+{
+    resultado = numero * multiplicador;
+    Console.WriteLine($"{numero} x {multiplicador}={resultado}");
+    multiplicador++;
+}
+{
+    Console.WriteLine("fim da tabuada");
+    Console.WriteLine("----------------");
+    Console.WriteLine();
+}
